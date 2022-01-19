390b7d9f05d1cba582a777d48e176f84d6128392
Shaktiswarup07 <shaktimohapatra07@gmail.com>
2022-01-19 22:26:21 +0530

    adding diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    int d;
+    cin>>d;
+    
+    
+    for(int i=0;i<d;i++){
+        cin>>a>>b;
+        cout<<"The diff is "<< a-b<<endl;
+
+    }
+
+
+return 0;
+
+}
